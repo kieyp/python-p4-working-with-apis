c8687d34f57818740ea9243195ca44ca773e2d9d
Boniface <cheruiyotbony@gmail.com>
2024-03-09 14:33:59 +0300

    tested

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,14 +1,27 @@
-
 import requests
 import json
 
 class GetPrograms:
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    def get_programs(self):
+        URL = "https://data.cityofchicago.org/resource/ydr8-5enu.json"
+        response = requests.get(URL)
+        return response.text
 
-    response = requests.get(URL)
-    return response.content
+    def program_schools(self):
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        print(programs)  # Print the response content for examination
+        for program in programs:
+            programs_list.append(program["agency"])
+        return programs_list
 
-programs = GetPrograms().get_programs()
-print(programs)
+# Instantiate the class
+program_instance = GetPrograms()
+
+# Call the method to get program schools
+programs_schools = program_instance.program_schools()
+
+# Print out the unique schools
+for school in set(programs_schools):
+    print(school)
